b6c9cd9f1b52296d4682014cbabf5cb7606b55b6
remvst <remvst@gmail.com>
2018-04-29 11:14:32 -0400

    Missing interpolation pool

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ module.exports.DynamicTimeline = require('./src/dynamic-timeline');
 module.exports.Timeline = require('./src/timeline');
 module.exports.Loop = require('./src/loop');
 module.exports.Easing = require('./src/easing');
+module.exports.InterpolationPool = require('./src/interpolation-pool');
