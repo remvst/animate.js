3ef16cfea5a7278b3347edd470d6b7d203853fb1
remvst <remvst@gmail.com>
2018-04-29 11:05:15 -0400

    Added main file

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports.Animation = require('src/animation');
+module.exports.DynamicTimeline = require('src/dynamic-timeline');
+module.exports.Timeline = require('src/timeline');
+module.exports.Loop = require('src/loop');
+module.exports.Easing = require('src/easing');
