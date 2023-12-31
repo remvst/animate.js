cff5ea88119bcd3c72922a2341b21ec1b0faf3e7
remvst <remvst@gmail.com>
2023-12-30 22:09:12 -0500

    Modernize a bit

--- lib/action.d.ts
+++
@@ -1,8 +0,0 @@
-import BaseAnimation from './base-animation';
-export default class Action extends BaseAnimation {
-    private _callback;
-    private _called;
-    constructor(callback: () => void);
-    cycle(elapsed: number): void;
-    get finished(): boolean;
-}
--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as Animation } from './animation';
-export { default as DynamicTimeline } from './dynamic-timeline';
-export { default as Timeline } from './timeline';
-export { default as Loop } from './loop';
-export * as Easing from './easing';
-export { default as InterpolationPool } from './interpolation-pool';
